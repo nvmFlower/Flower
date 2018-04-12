ed2285540bb2cbd6317ebdf321f28fdef0c112ac
nvmFlower <38124865+nvmFlower@users.noreply.github.com>
2018-04-12 13:26:18 +0700

    frontend

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :posts, dependent: :destroy
 
-  VALID_PHONE_REGEX = /^((\+841|01)[2689]|(\+849|09))[0-9]{8}$/
+  attr_accessor :remember_token
+  VALID_PHONE_REGEX = /\A((\+841|01)[2689]|(\+849|09))[0-9]{8}\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :address, presence: true
   validates :name, presence: true, length: {maximum: Settings.maximum_name}
@@ -13,4 +14,35 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: false}
   validates :phone, presence: true, uniqueness: true,
     format: {with: VALID_PHONE_REGEX}
+  has_secure_password
+  validates :password, presence: true,
+    length: {minimum: Settings.password.min_length}, allow_nil: true
+
+  class << self
+    def digest string
+        if cost = ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+   def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 end
